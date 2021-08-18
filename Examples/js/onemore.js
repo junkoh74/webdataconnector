5020f8e4634d5b70d79aa6db11586ef30d4854e3
junkoh74 <rhrudwns88@gmail.com>
2021-08-18 14:09:27 +0900

    test

--- Examples/js/onemore.js
+++ Examples/js/onemore.js
@@ -8,29 +8,9 @@
             id: "market",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "candle_acc_trade_volume",
-	alias: "volume",
-            dataType: tableau.dataTypeEnum.float
-        }, {
             id: "candle_date_time_kst",
             alias: "date",
             dataType: tableau.dataTypeEnum.date
-        }, {
-            id: "change_price",
-            alias: "price change",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "change_rate",
-            alias: "rate",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "high_price",
-            alias: "price high",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "low_price",
-            alias: "price low",
-            dataType: tableau.dataTypeEnum.float
         }];
 
         var tableSchema = {
@@ -51,13 +31,8 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "market": feat[i].market,
-                    "candle_acc_trade_volume": feat[i].properties.candle_acc_trade_volume,
-                    "candle_date_time_kst": feat[i].properties.candle_date_time_kst,
-                    "change_price": feat[i].properties.change_price,
-                    "change_rate": feat[i].properties.change_rate,
-                    "high_price": feat[i].properties.high_price,
-                    "low_price": feat[i].properties.low_price
+                    "market": onemore.json[i].market,
+                    "candle_date_time_kst": onemore.json[i].candle_date_time_kst,
                 });
             }
 
@@ -71,7 +46,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "upbit 180days data"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
